e3b1d7cf3ac4c6ac98e61b5667ab570ea7c10fe7
HoangHH <se180557hahuyhoang@gmail.com>
2025-06-22 20:34:11 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import AppRoutes from './routes/AppRoutes';
-import { VaccinationProvider } from './context/VaccinationContext';
 
 function App() {
-  return (
-    <VaccinationProvider>
-      <AppRoutes />
-    </VaccinationProvider>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
+
